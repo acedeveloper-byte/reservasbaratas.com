8d36d610b93288c07cc85e3f05f036db4e9838f4
Mayank kumar <mayank.kumarace@gmail.com>
2025-06-16 09:49:07 +0530

    added changes

--- pages/_document.js
+++ pages/_document.js
@@ -4,17 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Google tag (gtag.js) */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-17195637462"
-        ></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17195637462">
+        </script>
         <script>
-            { ` window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-17195637462');`}
-</script>
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17195637462');`}
+        </script>
       </Head>
       <body>
         <Main />
